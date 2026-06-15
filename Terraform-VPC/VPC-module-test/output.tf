33878e8c1fc4175151a57e067eddae2f6b4ed8ce
balumopuriDSO <balumopuri004@gmail.com>
2026-06-15 20:52:08 +0000

    added Terraform VPC parameter config

---
+++ Terraform-VPC/VPC-module-test/output.tf
@@ -0,0 +1,11 @@
+# output "azs_info" {
+#     value = module.vpc.azs_info
+# }
+
+# output "subnet_info" {
+#     value = module.vpc.subnet_info
+# }
+
+output "vpc_id" {
+  value = module.vpc.vpc_id
+}
