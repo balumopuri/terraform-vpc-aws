d832d407a667d067811d27db354813b48ed5523a
balumopuri <balumopuri19@gmail.com>
2026-06-25 03:34:39 +0530

    added backend

--- expense-infra-dev/60.backend.tf/data.tf
+++ expense-infra-dev/60.backend.tf/data.tf
@@ -1,12 +1,12 @@
 data "aws_ami" "joindevops" {
   most_recent = true
 
+  owners = ["973714476881"]
+
   filter {
     name   = "name"
-    values = ["RHEL-9-DevOps-Practice*"]
+    values = ["Redhat-9-DevOps-Practice"]
   }
-
-  owners = ["973714476881"]
 
   filter {
     name   = "root-device-type"
@@ -18,7 +18,6 @@ data "aws_ami" "joindevops" {
     values = ["hvm"]
   }
 }
-
 
 data "aws_ssm_parameter" "backend_sg_id" {
   name = "/${var.project_name}/${var.environment}/backend_sg_id"
